4240659a70dd80bf3d7812f13960f0238b766278
Rails Udemy Joge <jdiaz@kwantec.com>
2017-06-12 14:32:37 -0500

    create new template, action and route

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ AlphaBlog::Application.routes.draw do
   root 'welcome#home'
   get 'about', to: 'welcome#about'
 
+  resources :articles
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
